3ed0b5c9641f689a903075c6b33ea0f512332c85
SUD <sudhansu.ace@gmail.com>
2023-01-19 21:19:34 +0530

    Feature: Initialized Project

---
+++ src/main/java/com/recharger/admin/entity/Issuer.java
@@ -0,0 +1,27 @@
+package com.recharger.admin.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "issuer", uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
+public class Issuer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+}
